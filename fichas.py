5126bed86955655332d8d6489022c6c768522155
Jeferson <jeffher789@gmail.com>
2026-08-16 17:54:08 -0500

    Actualizacion a las funciones de colision y deteccion

--- fichas.py
+++ fichas.py
@@ -1,4 +1,7 @@
 import random
+import pygame
+import math
+
 class Bloque:
     ancho = 30
     def __init__(self,x,y,color):
@@ -16,151 +19,80 @@ class Bloque:
 
 class Pieza:
 
-    listPie = ['eled','elei','cuadrado',
-                'l', 't','z','s']
-    piezas = { 
-        'eled': [ [ [1,0],[1,0],[1,1]],
-                 [ [0,0,1],[1,1,1]],
-                 [ [1,1],[0,1],[0,1]],
-                 [ [1,1,1],[1,0,0]]
-                 ],
-        'elei': [ [[0,1],[0,1],[1,1]],
-                  [ [1,1,1],[0,0,1]],
-                  [ [1,1],[1,0],[1,0]],
-                  [ [1,0,0],[1,1,1]]
-                 ],
-        'cuadrado': [[ [1,1], [1,1]]],
-        'l': [ [[1,0], [1,0], [1,0], [1,0]],
-               [ [1,1,1,1]]
-               ],
-        't': [[ [1,1,1],[0,1,0]],
-              [ [1,0],[1,1],[1,0]],
-              [ [0,1,0],[1,1,1]],
-              [ [0,1],[1,1],[0,1]]
-              ],
-        'z': [ [[1,1,0],[0,1,1]],
-               [ [0,1],[1,1],[1,0]]
-              ],
-        's': [[ [0,1,1],[1,1,0]],
-              [ [1,0],[1,1],[0,1]]
-              ]
-        
-    }
-
-    colores = { 
-        'eled': 'yellow',
-        'elei': 'red',
-        'cuadrado': 'blue',
-        'l': 'cyan',
-        't': 'green',
-        'z': (218,238,113),
-        's': (244,141,73)
-        
-    }
 
 
-    def __init__(self,x,y,piece,colour):
-        self.x = x
-        self.y = y
+
+    def __init__(self,x,y,piece,bloques):
+        self.bloques = bloques
         self.pieza = piece
-        self.color = colour
-
-  
-
+        self.color = colores[piece]
+        self.forma = matrices[piece]
 
 
-   
-    def dibujar(self,rota):
-        lista = []
-        if self.pieza == 'cuadrado':
-            rota = 0
-        elif self.pieza == 'l':
-           
-            if rota%2 == 0:
-                rota =1
-            else:
-                rota = 0
-        elif self.pieza == 'z': 
 
-            if rota%2 == 0:
-                rota =1
-            else:
-                rota = 0
-        elif self.pieza == 's': 
-            if rota%2 == 0:
-                rota =1
-            else:
-                rota = 0
-        
-        for i in range (0,len(self.piezas.get(self.pieza)[rota]),1):
-            for j in range (0,len(self.piezas.get(self.pieza)[rota][0]),1):
-                valor = 30*self.piezas.get(self.pieza)[rota][i][j]*(j+1)
-                if valor != 0:
-                    m = len(self.piezas.get(self.pieza)[rota][0])
-                    n = len(self.piezas.get(self.pieza))
-                    #print("i va hasta "+str(n) +" j va hasta "+str(m))
-                    cuadro = Bloque(30*self.piezas.get(self.pieza)[rota][i][j]*(j+1)+self.x,30*self.piezas.get(self.pieza)[rota][i][j]*(i+1)+self.y,self.color)
-                    lista.append(cuadro)
-                else:
-                    pass
-        #print("Lar lista "+str(len(lista)))
-        return lista
-    
-    def rotar(self,rota,lista,x,y):
-        if self.pieza == 'cuadrado':
-            rota = 0
-        elif self.pieza == 'l':
-            
-            if rota%2 == 0:
-                rota =1
-            else:
-                rota = 0
-        elif self.pieza == 'z': 
-            if rota%2 == 0:
-                rota =1
-            else:
-                rota = 0
-        elif self.pieza == 's': 
-            if rota%2 == 0:
-                rota =1
-            else:
-                rota = 0
-        increm = 0
-        forma = self.piezas.get(self.pieza)[rota]
-        for i in range(0,len(forma),1):
-            for j in range(0,(len(forma[0])),1):
-                # [[1,1,0],[0,1,1]]
-                # [ [0,1],[1,1],[1,0]]
-                value = forma[i][j]
-                if value == 1:
-                    lista[increm].x = forma[i][j]*(j+x)*30
-                    lista[increm].y = forma[i][j]*(i+y)*30
-                    increm+=1
-        return lista
+    def rotar(self, width):
 
-    def maxX(self,lista):
-        max = 0
-        for objeto in lista:
-            x = objeto.x
-            if x> max:
-                max = x
-        return max
-    def minX(self,lista):
-        min = 1000
-        for objeto in lista:
-            x = objeto.x
-            if x< min:
-                min = x
-        return min
-    
-    def minY(self,lista):
-        min = 0
-        for objeto in lista:
-            x = objeto.y
-            if x> min:
-                min = x
-        return min
-    
+        minX = 1000
+        minY = 1000
+        maxX = 0
+        for bloque in self.bloques:
+            x = bloque.x
+            y = bloque.y
+            if x <= minX:
+                minX = x
+            if y <= minY:
+                minY = y
+            if x >= maxX:
+                maxX = x
+
+        forma =  [list(row) for row in zip(*self.forma[::-1])]
+        nums = []
+        i = 0
+        for lista in forma:
+            for elemento in lista:
+                if elemento == 1:
+                    nums.append(int(minX+ i*30))
+                i +=1
+        maximo = max(nums)
+        if  maximo >= width - 180:
+            self.forma = [list(row) for row in zip(*self.forma[::-1])]
+
+            k = 0
+            j = 0
+            i = 0
+
+
+            for lista in self.forma:
+
+                for elemento in reversed(lista):
+
+                    if elemento == 1:
+                        self.bloques[k].x =  maxX - i*30
+                        self.bloques[k].y =  minY + j*30
+                        k+=1
+                    i+=1
+                j+=1
+                i = 0
+        else:
+            self.forma = [list(row) for row in zip(*self.forma[::-1])]
+
+            k = 0
+            j = 0
+            i = 0
+
+            for lista in self.forma:
+
+                for elemento in lista:
+
+                    if elemento == 1:
+
+                        self.bloques[k].x = minX + i * 30
+                        self.bloques[k].y = minY + j * 30
+
+                        k += 1
+                    i += 1
+                i = 0
+                j += 1
 
 class generar:
     flag = 0
@@ -191,9 +123,6 @@ class generar:
             
         }
 
-    listPie = ['eled','elei','cuadrado',
-                    'l', 't','z','s']
-
     def randomPieza(self):
             pieza = random.choice(self.listPie)
             return pieza    
@@ -203,8 +132,165 @@ class generar:
     
         
     
+formas = ['eled','elei','cuadrado',
+                    'l', 't','z','s']
 
-def nueva (x1,x2,x3,x4,y1,y2,y3,y4):
-    pieza = Pieza(x1,x2,x3,x4+30,y1,y2,y3,y4)
-    return pieza
+colores = {
+    'eled': (119, 116, 6),
+    'elei': (120, 5, 14),
+    'cuadrado': (11, 109, 138),
+    'l': (19, 130, 55),
+    't': (123, 148, 27),
+    'z': (105, 22, 29),
+    's': (41, 73, 39)
 
+}
+matrices = {
+    'eled': [[1, 0], [1, 0], [1, 1]],
+    'elei': [[0, 1], [0, 1], [1, 1]],
+    'cuadrado': [[1, 1], [1, 1]],
+    'l': [[1, 0], [1, 0], [1, 0], [1, 0]],
+    't': [[1, 1, 1], [0, 1, 0]],
+    'z': [[1, 1, 0], [0, 1, 1]],
+    's': [[0, 1, 1], [1, 1, 0]]
+
+}
+
+
+def bloques(forma,color,x,y):
+    lista = []
+
+    for i in range(len(forma)):
+        for j in range(len(forma[0])):
+            valor = 30 * forma[i][j] * (j + 1)
+            if valor != 0:
+
+                cuadro = Bloque(30 * forma[i][j] * (j + 1) + x, 30 * forma[i][j] * (i + 1) + y,
+                                color)
+                lista.append(cuadro)
+            else:
+                pass
+    return lista
+
+def rellenar(screen,x,y):
+    pygame.draw.line(screen, (34, 29, 45), [x[0], x[1]], [y[0],y[1]], 1)
+
+def tablero(width, height):   # table = tablero(int(width//30),(int(height+60)//30))
+
+    matrix = []
+    for i in range (0,int(height),1):
+        matr = []
+        for j in range (0,int(width),1):
+            matr.append(0)
+        matrix.append(matr)
+    return matrix
+
+def unos(tablero,row,col):
+
+    tablero[row][col] = 1
+    return tablero
+
+def imprimirTablero(tablero):
+    for row in tablero:
+        for col in row:
+            print(col, end=" ")
+        print()
+
+# collis necesita objetos pieza
+def collis(piece, desfase, width, height,tablero):
+
+    if isinstance(piece, Pieza):
+        for bloque in piece.bloques:
+            #i=1
+            x = int(bloque.x + desfase[0]*30)
+            y = int(bloque.y + desfase[1]*30)
+            #print('x ' + str(x//30) + ' y ' + str(y//30))
+            #print('x ' + str(x//30) + ' y ' + str(y//30))
+            #print(tablero[int(y//30)][int(x//30)])
+            #print('bloque num '+str(i))
+            #i+=1
+            # y < 0 or y >= height-35
+            if x < 0 or x >= width-30 or y < 0 or y >= height-30:
+                return False
+    else:
+        x = int(piece.x + desfase[0] * 30)
+        y = int(piece.y + desfase[1] * 30)
+        if x < 0 or x >= width - 30 or y < 0 or y >= height - 30:
+            return False
+
+    return True
+
+def collisUnos (objeto, desfase,tablero):
+    if isinstance(objeto, Pieza):
+
+        for bloque in objeto.bloques:
+            x = int(bloque.x // 30) + desfase[0]
+            y = int(bloque.y // 30) + desfase[1]
+            if desfase[0] !=0 and tablero[y][x] == 1:
+                return False
+            elif desfase[1] !=0 and tablero[y][x] == 1:
+                return False
+    else:
+
+        x = int(objeto.x // 30) + desfase[0]
+        y = int(objeto.y // 30) + desfase[1]
+        if desfase[0] !=0 and tablero[y][x] == 1:
+            return False
+        elif desfase[1] !=0 and tablero[y][x] == 1:
+            return False
+
+    return True
+
+
+
+
+
+def verificarUnos(tablero,width,fila):        # lista de las filas llenas
+
+    total = tablero[fila].count(1)
+    if total == width//30:
+
+        return True
+
+    return False
+
+def eliminarUnos(listaObjects,fila):
+    borrar = []
+    for i in range (0,len(listaObjects),1):
+        y = int(listaObjects[i].y/30)
+        if y in fila:
+            borrar.append(i)
+    borrar.sort(reverse=True)
+    for elem in borrar:
+        listaObjects.pop(elem)
+    return listaObjects
+
+def actualizarTablero(tablero,fila,width):  # cambiar a ceros los valores de las filas llenas
+
+    for i in range(0,width//30,1):
+        tablero[fila][i] = 0
+    return tablero
+
+def bajarObj(lineas,listaObj):
+
+    for bloques in listaObj:
+        lineaY = bloques.y
+        for linea in lineas:
+            if lineaY < linea*30:
+                bloques.y = bloques.y + 30
+    return listaObj
+
+def bajarUnos(listaObj, actual,width, height,table):
+    tama = int(len(table))
+    table = tablero(int(width//30),int((height+30)//30))
+    for i in range(0, width // 30, 1):  # cambiado
+        table = unos(table, tama - 1, i)
+    for bloque in listaObj:
+        x = int(bloque.x//30)
+        y = int(bloque.y//30)
+        table = unos(table,y,x)
+    for bloque in actual:
+        x = int(bloque.x//30)
+        y = int(bloque.y//30)
+        table = unos(table,y,x)
+    return table
--- pruebas.py
+++ pruebas.py
@@ -1,42 +1,47 @@
 import pygame
 from fichas import *
-from funciones import *
+
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((450, 900))  #pygame.display.set_mode((1280, 720))
+info = pygame.display.Info()
+width = int((info.current_w)//3)
+CENTRO = ((width/2)//30)*30
+height = int((info.current_h)*0.8)
+
+screen = pygame.display.set_mode((width, height))  #pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
+
 running = True
-tablero = tablero()
-for i in range(0,15,1):
-    tablero = unos(tablero,30,i)
-gen = generar(120,-30)
-piece =  gen.randomPieza()
-color = gen.colores.get(piece)
-actual = gen.generaPiece(piece,color)
+tablero = tablero(int(width//30),int((height+30)//30))
+
+for i in range(0,width//30,1):  # cambiado
+    tablero = unos(tablero,int(len(tablero)-1),i) # cambiado
+
+piece =  random.choice(formas)
+color = colores[piece]
+lista = bloques(matrices[piece],color,CENTRO,0)
+actual = Pieza(int(((width//30)//3)*30),0,piece,lista)
 rotacion = 0
-lista = actual.dibujar(rotacion)
+
 listaO =[]
 FALL = pygame.USEREVENT + 1
-pygame.time.set_timer(FALL, 200)
+pygame.time.set_timer(FALL, 500)
 
-for objeto in lista: # se cambio el agregar la lista completa a listaO
-    listaO.append(objeto)
+#for objeto in lista: # se cambio el agregar la lista completa a listaO
+    #listaO.append(objeto)
+
 key_state = False
 m =0
-print(len(listaO)/4)
+imprimirTablero(tablero)
 while running:
-    
-     
-    
+
+
+
+
     # Falta organizar la verificacion de la rotacion que no se salga de la pantalla
 
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
-
-    fichaList = posTabl(lista)
-    coll = collis(tablero, fichaList)  # devuelve una lista con tuplas de coordenadas
-    velocidad = veloc.get(actual.pieza)
-
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -44,234 +49,103 @@ while running:
         elif event.type == pygame.KEYDOWN:
 
             if event.key == pygame.K_x:
-                var = lista[:]
-                siguiente = ladosPieza(var,actual,rotacion,1)
+                actual.rotar(width)
 
-
-                if siguiente == True:
-
-                    rotacion+=1
-                        
-                    if rotacion>3:
-                        rotacion = 0
-                        
-                    fichaList = posTabl(lista)
-                    minX = minValues(fichaList,'tup',0)
-                    minY = minValues(fichaList,'tup',1)
-                    lista = actual.rotar(rotacion,lista,minX,minY)
-            
             elif event.key == pygame.K_z:
-                var = lista[:]
-                anterior = ladosPieza(var,actual,rotacion,-1)
-                if anterior== True:
+                pass
 
-                    rotacion-= 1
-                    if rotacion<0:
-                        rotacion = 3
-                
-                    fichaList = posTabl(var)
-                    minX = minValues(fichaList, 'tup', 0)
-                    minY = minValues(fichaList, 'tup', 1)
-                    lista = actual.rotar(rotacion,lista,minX,minY)
+            elif event.key == pygame.K_LEFT and collis(actual,(-1,0),width,height,tablero):
+
+                    actual.bloques[0].x -= 30
+                    actual.bloques[1].x -= 30
+                    actual.bloques[2].x -= 30
+                    actual.bloques[3].x -= 30
 
 
-            elif event.key == pygame.K_LEFT:
+            elif event.key == pygame.K_RIGHT and collis(actual,(1,0),width,height,tablero):
 
-                valcol = []
-                valuesFich = []
-                band = False
-                cont = 0
-
-                for tupla in fichaList:
-                    valuesFich.append((tupla[0] - 30, tupla[1]))
-
-                for tupla in coll:
-
-                    if tupla[0] != -1:
-                        valcol.append((tupla[0] * 30, tupla[1] * 30))
-
-                for tuple in valuesFich:
-
-                    if tuple in valcol or tuple[0] < 0:
-
-                        band = True
-
-                    else:
-
-                        band = False
-                        cont+=1
-
-                if band == False and cont == 4:
-                    lista[0].x -= 30
-                    lista[1].x -= 30
-                    lista[2].x -= 30
-                    lista[3].x -= 30
-
-            elif event.key == pygame.K_RIGHT:
-
-                valcol = []
-                valuesFich = []
-                band = False
-                cont = 0
-
-                for tupla in fichaList:
-                    valuesFich.append((tupla[0] + 30, tupla[1]))
-
-                for tupla in coll:
-
-                    if tupla[0] != -1:
-                        valcol.append((tupla[0] * 30, tupla[1] * 30))
-
-                for tuple in valuesFich:
-
-                    if tuple in valcol or tuple[0] == 450:
-
-                        band = True
-
-                    else:
-
-                        band = False
-                        cont += 1
-
-                if band == False and cont == 4:
-
-                    lista[0].x += 30
-                    lista[1].x += 30
-                    lista[2].x += 30
-                    lista[3].x += 30
+                    actual.bloques[0].x += 30
+                    actual.bloques[1].x += 30
+                    actual.bloques[2].x += 30
+                    actual.bloques[3].x += 30
 
 
                 # fill the screen with a color to wipe away anything from last frame
-            elif event.key == pygame.K_DOWN:
 
-                valcol = []
-                valuesFich = []
-                band = False
+            elif event.key == pygame.K_DOWN and collisUnos(actual,(0,1),tablero):
 
-                for tupla in fichaList:
-                    valuesFich.append((tupla[0], tupla[1] + 30))
-
-                for tupla in coll:
-
-                    if tupla[0] != -1:
-                        valcol.append((tupla[0] * 30, tupla[1] * 30))
-
-                for tuple in valuesFich:
-
-                    if tuple not in valcol:
-
-                        band = True
-
-                    else:
-
-                        band = False
-
-                if band:
-
-                    lista[0].y += 30
-                    lista[1].y += 30
-                    lista[2].y += 30
-                    lista[3].y += 30
-
-        elif event.type == FALL:
-
-            valcol = []
-            valuesFich = []
-            band = False
+                    actual.bloques[0].y += 30
+                    actual.bloques[1].y += 30
+                    actual.bloques[2].y += 30
+                    actual.bloques[3].y += 30
 
 
-            for tupla in fichaList:
 
-                valuesFich.append(( tupla[0] , tupla[1]+30 ))
-
-            for tupla in coll:
-
-                if tupla[0] != -1:
-
-                    valcol.append(( tupla[0] * 30 , tupla[1] * 30 ))
-
-            for tuple in valuesFich:
-
-                    if tuple in valcol:
-
-                        for x, y in fichaList:
-                            i = int(x / 30)
-                            j = int(y / 30)
-                            tablero = unos(tablero, j, i)
-
-                        nums = verificarUnos(tablero)
-                        if len(nums) != 0:
-
-                             listaO = eliminarUnos(listaO, nums)
-                             tablero = actualizarTablero(tablero, nums)
-                             listaO = bajarObj(nums, listaO)
-                             tablero = bajarUnos(listaO)
+        elif event.type == FALL :
 
 
-                        piece = gen.randomPieza()
-                        color = gen.colores.get(piece)
-                        actual = gen.generaPiece(piece, color)
-                        lista = []
-                        lista = actual.dibujar(rotacion)
 
-                        for objeto in lista:
-                            listaO.append(objeto)
+            if collisUnos(actual, (0,1), tablero):  # verificar si hay ceros debajo
 
-                        valcol = {}
-                        valuesFich = {}
+                actual.bloques[0].y += 30
+                actual.bloques[1].y += 30
+                actual.bloques[2].y += 30
+                actual.bloques[3].y += 30
+
+            else:
+
+                lislin = []
+                for bloque in lista:
+
+                    tablero = unos(tablero,int(bloque.y//30),int(bloque.x//30)) # llenar unos de fichas
+                    estado = verificarUnos(tablero,width,int(bloque.y//30))  # verificar si esta llena la linea
+
+                    if estado:
+                       lislin.append(int(bloque.y//30))
+
+                if estado:
+                       #tablero = actualizarTablero(tablero,int(bloque.y//30),width)  # borra 1 y llena 0
+                    listaO = eliminarUnos(listaO,lislin)  # borrar objetos bloques
+                    actual.bloques = eliminarUnos((actual.bloques), lislin)
+                    actual.bloques = bajarObj(lislin, actual.bloques)
+                    listaO = bajarObj(lislin,listaO)
+                    tablero = bajarUnos(listaO,actual.bloques,width,height,tablero)
+                for objeto in actual.bloques:  # se cambio el agregar la lista completa a listaO
+                    listaO.append(objeto)
+                lista = 0
+                piece = random.choice(formas)
+                color = colores[piece]
+                lista = bloques(matrices[piece], color, CENTRO, 0)
+                actual = Pieza(int(((width // 30) // 3) * 30), 0, piece, lista)
+                imprimirTablero(tablero)
+                print('---------------------------------------------------------')
 
 
-                        imprimirTablero(tablero)
 
-                        break
 
-                    else:
 
-                        bajar = True
-                        band = True
-
-            if band:
-
-                lista[0].y += 30
-                lista[1].y += 30
-                lista[2].y += 30
-                lista[3].y += 30
 
     screen.fill((49,38,75))
+
+    for bloque in actual.bloques:
+        pygame.draw.rect(screen, bloque.color, [bloque.x, bloque.y, 30, 30])
 
     for coor in listaO: # for coor in objeto:
         pygame.draw.rect(screen,coor.color,[coor.x,coor.y,30,30])
 
-    for i in range (0,450,30):
-        rellenar(screen,(i,0),(i,900))
-    for i in range(0,900,30):
-        rellenar(screen,(0,i),(450,i))
 
 
-    if 1 in tablero[3]:
-        listaO.clear()
+    for i in range (0,int(width),30):
+        rellenar(screen,(i,0),(i,height))
+    for i in range(0,int(height),30):
+        rellenar(screen,(0,i),(width,i))
 
-        piece =  gen.randomPieza()
-        color =  gen.colores.get(piece)
-        actual = gen.generaPiece(piece,color)
-        lista = []
-        lista = actual.dibujar(rotacion)
-        for objeto in lista:
-            listaO.append(objeto)
 
-        imprimirTablero(tablero)
-        for i in range(0,15,1):
-            for j in range(0,30,1):
-                tablero[j][i]=0
-        for i in range(0,15,1):
-            tablero = unos(tablero,30,i)
     # flip() the display to put your work on screen
     pygame.display.flip()
-
     # limits FPS to 60
     # dt is delta time in seconds since last frame, used for framerate-
     # independent physics.
-    dt = clock.tick(60)/1000
-    
-    
+
+
 pygame.quit()
